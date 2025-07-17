214a5476f1146bedfffca8dbc37a4e703c710253
Shravani1603 <149895354+Shravani1603@users.noreply.github.com>
2025-07-17 19:41:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Node.js deployed on EKS with Jenkins CI/CD!");
+  res.send("Hello Shravani, this is Node.js deployed on EKS with Jenkins CI/CD!");
 });
 
 app.listen(port, () => {
